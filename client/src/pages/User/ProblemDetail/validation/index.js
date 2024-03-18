04181c92e43e5e270ce2ca1800ba9018753eb021
leho-dev <phanleho002@gmail.com>
2024-03-18 09:09:55 +0700

    refactor: validate 'main' run & submit

--- client/src/pages/User/ProblemDetail/validation/index.js
+++ client/src/pages/User/ProblemDetail/validation/index.js
@@ -1,43 +1,45 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const runSchema = Joi.object({
-    problem: Joi.object({
-        _id: Joi.string().hex().length(24).required().trim().strict(),
-        author: Joi.string().hex().length(24).required().trim().strict(),
-        uuid: Joi.string()
-            .guid({ version: "uuidv4" })
-            .required()
-            .trim()
-            .strict(),
-    }),
-    code: Joi.string().required().trim().strict().label("Code editor"),
-    testcases: Joi.object({
-        input: Joi.array().items(Joi.any()).required(),
-        output: Joi.array().items(Joi.any()).required(),
-    }).required(),
+  problem: Joi.object({
+    _id: Joi.string().hex().length(24).required().trim().strict(),
+    author: Joi.string().hex().length(24).required().trim().strict(),
+    uuid: Joi.string().guid({ version: 'uuidv4' }).required().trim().strict(),
+  }),
+  code: Joi.string()
+    .required()
+    .trim()
+    .strict()
+    .regex(/main/)
+    .label('Code editor'),
+  testcases: Joi.object({
+    input: Joi.array().items(Joi.any()).required(),
+    output: Joi.array().items(Joi.any()).required(),
+  }).required(),
 });
 
 export const submitSchema = Joi.object({
-    problem: Joi.object({
-        _id: Joi.string().hex().length(24).required().trim().strict(),
-        author: Joi.string().hex().length(24).required().trim().strict(),
-        uuid: Joi.string()
-            .guid({ version: "uuidv4" })
-            .required()
-            .trim()
-            .strict(),
-    }),
-    code: Joi.string().required().trim().strict().label("Code editor"),
+  problem: Joi.object({
+    _id: Joi.string().hex().length(24).required().trim().strict(),
+    author: Joi.string().hex().length(24).required().trim().strict(),
+    uuid: Joi.string().guid({ version: 'uuidv4' }).required().trim().strict(),
+  }),
+  code: Joi.string()
+    .required()
+    .trim()
+    .strict()
+    .regex(/main/)
+    .label('Code editor'),
 });
 
 export const handleValidate = (schema, data) => {
-    const { error } = schema.validate(data, { abortEarly: false });
-    if (error) {
-        let errors = "";
-        error.details.forEach((err) => {
-            errors += `${err.message}. `;
-        });
-        return errors;
-    }
-    return null;
+  const { error } = schema.validate(data, { abortEarly: false });
+  if (error) {
+    let errors = '';
+    error.details.forEach((err) => {
+      errors += `${err.message}. `;
+    });
+    return errors;
+  }
+  return null;
 };
