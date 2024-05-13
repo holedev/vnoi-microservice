160c593d56574c308776eae922f06dcaefea7338
leho-dev <phanleho002@gmail.com>
2024-05-13 12:51:22 +0700

    fix: static file with nginx

--- server/media/src/api/services/Image.js
+++ server/media/src/api/services/Image.js
@@ -1,7 +1,8 @@
+import { _PROCESS_ENV } from "../../configs/env/index.js";
 import { httpStatusCodes } from "../responses/httpStatusCodes/index.js";
 import { gRPCRequest } from "./gRPC.js";
 
-const _IMAGE_PATH = "http://localhost:9004/images/";
+const _IMAGE_PATH = _PROCESS_ENV.STATIC_FILE_URL + "images/";
 
 const ImageService = {
   createCoverImage: async (req, res) => {
