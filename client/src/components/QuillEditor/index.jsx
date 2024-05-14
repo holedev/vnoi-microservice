d6b1a8ec98837ceb011f1dd44796efa56a35a3d8
leho-dev <phanleho002@gmail.com>
2024-05-14 09:10:50 +0700

    perf: react-quill instead of react-quilljs

--- client/src/components/QuillEditor/index.jsx
+++ client/src/components/QuillEditor/index.jsx
@@ -1,30 +1,39 @@
-import { useQuill } from 'react-quilljs';
-import BlotFormatter from 'quill-blot-formatter/dist/BlotFormatter';
+// import { useQuill } from 'react-quilljs';
+// import BlotFormatter from 'quill-blot-formatter/dist/BlotFormatter';
+// import 'react-quill/dist/quill.snow.css';
+// import { useEffect } from 'react';
+
+// function QuillEditor({ value, setValue }) {
+//   const { quill, quillRef, Quill } = useQuill({
+//     modules: { blotFormatter: {} },
+//   });
+
+//   if (Quill && !quill) {
+//     Quill.register('modules/blotFormatter', BlotFormatter);
+//   }
+
+//   useEffect(() => {
+//     if (quill) {
+//       if (value) {
+//         quill.clipboard.dangerouslyPasteHTML(value);
+//       }
+
+//       quill.on('text-change', (delta, oldContents, source) => {
+//         setValue(quillRef.current.firstChild.innerHTML);
+//       });
+//     }
+//   }, [quill, Quill]);
+
+//   return <div ref={quillRef} />;
+// }
+
+// export default QuillEditor;
+
+import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import { useEffect } from 'react';
 
 function QuillEditor({ value, setValue }) {
-  const { quill, quillRef, Quill } = useQuill({
-    modules: { blotFormatter: {} },
-  });
-
-  if (Quill && !quill) {
-    Quill.register('modules/blotFormatter', BlotFormatter);
-  }
-
-  useEffect(() => {
-    if (quill) {
-      if (value) {
-        quill.clipboard.dangerouslyPasteHTML(value);
-      }
-
-      quill.on('text-change', (delta, oldContents, source) => {
-        setValue(quillRef.current.firstChild.innerHTML);
-      });
-    }
-  }, [quill, Quill]);
-
-  return <div ref={quillRef} />;
+  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
 }
 
 export default QuillEditor;
