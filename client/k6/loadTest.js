7bf102b0edd74f2c7768b9767150f6b597a13e98
leho-dev <phanleho002@gmail.com>
2024-07-07 00:17:17 +0700

    ci: fix ci

--- client/k6/loadTest.js
+++ client/k6/loadTest.js
@@ -1,6 +1,7 @@
 import http from "k6/http";
 import { sleep } from "k6";
 
+// Change the base URL to the staging server
 const _BASE_URL = "https://vnoi-server-staging.undefine.tech/";
 
 export const options = {
